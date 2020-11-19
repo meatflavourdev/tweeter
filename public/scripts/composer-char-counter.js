2da3472d01a51f82243ceccf1077fbe0677b85dc
Jeremy Felix D <me@jeremyfelix.com>
2020-11-19 01:20:34 -0800

    Bugfix: Character counter fixed

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,10 +3,9 @@ const MAX_CHAR = 140;
 $(document).ready(function() {
   $('#tweet-text').on('input', function(event) {
     let charsleft = MAX_CHAR - event.target.value.length;
-    // console.log(MAX_CHAR - event.target.value.length + ' characters left');
-    const counter = $('#tweet-text + div output.counter');
+    const counter = $('#compose-counter');
     counter.html(charsleft);
-    if (charsleft <= 0) {
+    if (charsleft < 0) {
       counter.addClass('negative');
     } else {
       counter.removeClass('negative');
